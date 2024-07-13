652acd207ece61f1e88d0fa0969a3a74f1cc0772
Prabhudatta19 <prabhudatta.prof@gmail.com>
2024-07-13 22:29:45 +0530

    intial changes to the template

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,24 +7,24 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
+        <h2>Contact Me @</h2>
+        {/* <p>Feel free to reach out!</p> */}
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:prabhudatta.prof@gmail.com" target="_blank">prabhudatta.prof@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/prabhudatta-mohapatra/" target="_blank">prabhudatta-mohapatra</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://github.com/Prabhudatta19" target="_blank">Prabhudatta19</a>
         </li>
       </ul>
     </footer>
